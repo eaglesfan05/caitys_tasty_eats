314f9444a42c6dc04e17dc1dc485f5023daca391
Khalil <khalilhall1221@gmail.com>
2020-11-24 10:32:22 -0500

    fade in scroll button

--- public/assets/scripts/ind.js
+++ public/assets/scripts/ind.js
@@ -1,3 +1,4 @@
+
 var coll = document.getElementsByClassName('menu-section-header');
 var i;
 for(i = 0; i < coll.length; i++) {
@@ -28,3 +29,58 @@ for(i = 0; i< closeSideChoices.length; i++) {
         document.getElementById('side-options').style.display = 'none'
     })
 }
+
+var addButtonClicked = document.getElementsByClassName('add-item')
+var i;
+for(i = 0; i < addButtonClicked.length; i++){
+    addButtonClicked[i].addEventListener('click', function(event) {
+        var button = event.target;
+        var comfortItem = button.parentElement;
+        var title = comfortItem.getElementsByClassName('title')[0].innerText       
+        var price = comfortItem.getElementsByClassName('price')[0].innerText
+        alert(title + " " + price)
+        addItemToCart(title, price)
+        // get the element clicked
+        // get the value of the title, price
+        // set to variables
+        // get the div of the order message
+        // set the text to the value of the variables
+    })
+}
+
+function addItemToCart(title, price) {
+    let orderRow = document.createElement('div')
+
+    let orderContents = `
+    <div>${title} ${price} </div> ` 
+console.log(orderContents)
+
+orderRow.innerHTML += orderContents
+
+
+    let order = document.getElementsByClassName('orderHere')[0];
+
+//    orderRow.innerHTML = orderContents;
+   order.append(orderRow)
+    // console.log("fuckin" + item)
+    // order.push(item)
+    // console.log('this is the order' + order)
+    // let i;
+    // for(i = 0; i < order.length; i++) {
+    //     document.getElementById('orderHere').innerText = order[i]
+    // }
+   
+   
+    
+    
+}
+
+const toTop = document.querySelector('.scroll-top')
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset> 100) {
+        toTop.classList.add('active')
+    } else {
+        toTop.classList.remove('active')
+    }
+})
