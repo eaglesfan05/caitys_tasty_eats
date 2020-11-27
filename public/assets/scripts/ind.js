52988ac27cfd46f2af545fe1bddd440f1b3d9bb1
Khalil <khalilhall1221@gmail.com>
2020-11-27 17:29:00 -0500

    SOTFS

--- public/assets/scripts/ind.js
+++ public/assets/scripts/ind.js
@@ -1,5 +1,4 @@
 
-
 $(function() { 
 
 var coll = document.getElementsByClassName('menu-section-header');
@@ -16,11 +15,11 @@ for(i = 0; i < coll.length; i++) {
 
     })
 }
-var comboButtons = document.getElementsByClassName('combo');
+var comboButtons = document.getElementsByClassName('comboMeal');
 var i;
 for(i = 0; i< comboButtons.length; i++) {
     comboButtons[i].addEventListener('click', function() {
-        alert('click')
+        // alert('click')
         document.getElementById('side-options').style.display = 'block';
     }) 
 }
@@ -34,35 +33,73 @@ for(i = 0; i< closeSideChoices.length; i++) {
 }
 
 
-$('.hide-from-view').click(function() {
-    var text = '';
-    $('.hide-from-view:checked').each(function() {
-        text += "Order Item:" + $(this).val() + " | "
-    })
-
-    text =text.substring(0, text.length -1)
-    $('#orderHere').val(text)
-})
 
 
-// combo buttons
-$('.add-combo').click(function() {
-    var text = ''
-    var title = ''
-    var firstChoice = ''
-    var secondChoice = '';
+var item = ''
+
+var comboItem = ''
+var firstChoice = ''
+var secondChoice = ''
+var combo = comboItem + firstChoice + secondChoice
+function buildOrder(item, combo) {
+    // let order = '';
+    if (!combo) {
+        console.log(item)
+    }else {
+        console.log(combo)
     
-   
-    $('.firstChoice:checked').each(function() {
-        firstChoice+= "First Side: " + $(this).val()
-        console.log(firstChoice)
-    })
-    $('.secondChoice:checked').each(function() {
-        secondChoice+= "Second Side: " + $(this).val()
-        console.log(secondChoice)
+    }
+  
+}
+
+
+    $('.hide-from-view').click(function() {
+      
+        ;
+        $('.hide-from-view:checked').each(function() {
+            item += "Order Item:" + $(this).val() + " | "
+        })
+        buildOrder(item)
+        // item =item.substring(0, item.length -1)
+        // $('#orderHere').val(item)
     })
 
-})
+    $('.comboMeal').click(function() {
+      
+        ;
+        $('.comboMeal:checked').each(function() {
+            comboItem += "Combo Item:" + $(this).val() + " | "
+        })
+        buildOrder(comboItem)
+        // item =item.substring(0, item.length -1)
+        // $('#orderHere').val(item)
+    })
+    
+    
+    // combo buttons
+    $('.add-combo').click(function() {
+        
+        var text = ''   
+       
+        
+       
+        $('.firstChoice:checked').each(function() {
+            firstChoice+= "First Side: " + $(this).val()
+            buildOrder(firstChoice)
+        })
+        $('.secondChoice:checked').each(function() {
+            secondChoice+= "Second Side: " + $(this).val()
+            buildOrder(secondChoice)
+        })
+        // text += firstChoice + " " +  secondChoice
+        // text =text.substring(0, text.length -1)
+        
+    })
+    // console.log("item: " + item + " first: " + firstChoice + " second: " + secondChoice)
+    // order += item + firstChoice + secondChoice
+    // order = order.substring(0, order.length -1)
+    // $('#orderHere').val(order)
+
 
 
 
