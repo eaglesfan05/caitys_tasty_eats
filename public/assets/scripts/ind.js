fdd6688f39c551dc7083d07fca11b6f80406de06
Khalil <khalilhall1221@gmail.com>
2020-11-27 10:31:47 -0500

    div styling

--- public/assets/scripts/ind.js
+++ public/assets/scripts/ind.js
@@ -1,3 +1,4 @@
+// const e = require("express");
 
 var coll = document.getElementsByClassName('menu-section-header');
 var i;
@@ -17,7 +18,7 @@ var comboButtons = document.getElementsByClassName('combo');
 var i;
 for(i = 0; i< comboButtons.length; i++) {
     comboButtons[i].addEventListener('click', function() {
-        // alert('click')
+        alert('click')
         document.getElementById('side-options').style.display = 'block';
     }) 
 }
@@ -30,38 +31,95 @@ for(i = 0; i< closeSideChoices.length; i++) {
     })
 }
 
-var addButtonClicked = document.getElementsByClassName('add-item')
-var i;
-for(i = 0; i < addButtonClicked.length; i++){
-    addButtonClicked[i].addEventListener('click', function(event) {
-        var button = event.target;
-        var comfortItem = button.parentElement;
-        var title = comfortItem.getElementsByClassName('title')[0].innerText       
-        var price = comfortItem.getElementsByClassName('price')[0].innerText
-        // alert(title + " " + price)
-        addItemToCart(title, price)
-        // get the element clicked
-        // get the value of the title, price
-        // set to variables
-        // get the div of the order message
-        // set the text to the value of the variables
-    })
+// var addButtonClicked = document.getElementsByClassName('add-item')
+// var i;
+// for(i = 0; i < addButtonClicked.length; i++){
+//     var button = addButtonClicked[i]
+//     button.addEventListener('click', addItemToCart)
+// }
+
+function addItemToCart(event) {
+    event.preventDefault()
+    var button = event.target;
+    var menuItem = button.parentElement;
+    var title = menuItem.getElementsByClassName('title')[0].innerText;
+    var price = menuItem.getElementsByClassName('price')[0].innerText;
+
+    addTextToMessage(title, price)
+    
 }
 
-function addItemToCart(title, price) {
-    let orderRow = document.createElement('div')
+const addTextToMessage =(title, price) => {
 
-    let orderContents = `
-    <div>${title} ${price} </div> ` 
-console.log(orderContents)
+   
+    // alert(title+  price)
 
-orderRow.innerHTML += orderContents
+    // let textarea = document.createElement('textarea');
+    // textarea.setAttribute('name', 'comments')
+    
+    let orders = []
+    let order = {
+        id: Date.now(),
+        title: title,
+        price: price
+    }
+
+for(var i = 0; i < orders.length; i++) {
+    order = order[i]
+    orders.push(order)
+    console.log(orders)
+
+}
+   
+    // orders[orders.length] = title + price
+    // console.log(orders)
+    // for(var i = 0; i < orders.length; i++) {
+      
+        // document.getElementById('orderHere').value=
+        // `<div>${title} ${price}</div>` 
+    // }
+   
+  
+//    orders.push(item)
+
+//    orders.length(price)
+    // textarea.innerText =`${title} `
+    // let orderHere = document.getElementsByClassName('orderHere');
+    // orderHere.append(textarea2804)
+   
+// console.log('item here' + itemHere);
+
+console.log('order' + order.title )
+
+// Store
+localStorage.setItem("title", title);
+localStorage.setItem("price", price);
+
+// Retrieve
+// document.getElementById("orderHere").innerHTML = localStorage.getItem("title" + "price");
+
+    // for(var i = 0; i< order.length; i++) {
+        // console.log(order.length)
+        // alert(order[i])
+        // console.log(order)
+     
+        // let item = document.getElementById('orderHere')[0];
+        // item.value = order[i]
+    }
+    
+    // let orderRow = document.createElement('div')
+    // let order = document.getElementsByClassName('orderHere')[0];
+    // let orderContents = `
+    // <div class="cart-row"><span>${title}</span> ${" "} <span>${price}</span> </div> ` 
+// console.log(orderContents)
+
+// orderRow.innerHTML = orderContents
 
 
-    let order = document.getElementsByClassName('orderHere')[0];
+  
 
 //    orderRow.innerHTML = orderContents;
-   order.append(orderRow)
+//    order.append(orderRow)
     // console.log("fuckin" + item)
     // order.push(item)
     // console.log('this is the order' + order)
@@ -71,9 +129,39 @@ orderRow.innerHTML += orderContents
     // }
    
    
-    
-    
+  // alert('done')
+
+// }
+
+
+
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+let order = []
+
+for(var i; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('click', addToMessage)
 }
+
+function addToMessage(event) {
+    alert('checked')
+// let title = event.target.name;
+// console.log(title)
+}
+
+// for(const button of addItem) {
+//     button.addEventListener('click', ()=> {
+//         alert('click')
+//     })
+// }
+
+// addItem.forEach(function(checkbox) {
+//     checkbox.addEventListener('change', function(){
+//         alert('checked')
+//     })
+// })
+
 
 const toTop = document.querySelector('.scroll-top')
 
