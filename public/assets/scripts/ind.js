4483564143328cd90633222aa412dfe1dc321de3
Khalil <khalilhall1221@gmail.com>
2020-11-27 14:54:10 -0500

    get value from side buttons

--- public/assets/scripts/ind.js
+++ public/assets/scripts/ind.js
@@ -37,189 +37,34 @@ for(i = 0; i< closeSideChoices.length; i++) {
 $('.hide-from-view').click(function() {
     var text = '';
     $('.hide-from-view:checked').each(function() {
-        text += $(this).val() + ','
+        text += "Order Item:" + $(this).val() + " | "
     })
 
     text =text.substring(0, text.length -1)
     $('#orderHere').val(text)
 })
 
-// $('#orderButton').click(function(e) {
-//     e.preventDefault()
-  
-//     var order = [];
-//   $.each($('input[name="choices"]:checked'), () => {
-//       order.push($(this.val()))
-//       alert('order is ' + order.join(','))
-//   })
-//     // $('#orderHere').text(order)
-// })
-// get value of all checked buttons
 
-
-// let orderButton = document.getElementById('orderButton')
-// orderButton.addEventListener('click', function(e) {
-//    e.preventDefault();
-//    var order = []
-// const checkedButtons = document.querySelectorAll('input[name="choices"]:checked');
-// checkedButtons.forEach(function() {
-//     let selection = checkedButtons.values()
-//     console.log(selection)
-// })
-// })
-
-
-
-// checkedButtons.forEach((checkbox)=> {
-    // order.push(checkbox.value)
-    // console.log(order)
-// })
-// var addButtonClicked = document.getElementsByClassName('add-item')
-// var i;
-// for(i = 0; i < addButtonClicked.length; i++){
-//     var button = addButtonClicked[i]
-//     button.addEventListener('click', addItemToCart)
-// }
-
-// function addItemToCart(event) {
-//     event.preventDefault()
-//     var button = event.target;
-//     var menuItem = button.parentElement;
-//     var title = menuItem.getElementsByClassName('title')[0].innerText;
-//     var price = menuItem.getElementsByClassName('price')[0].innerText;
-
-//     addTextToMessage(title, price)
+// combo buttons
+$('.add-combo').click(function() {
+    var text = ''
+    var title = ''
+    var firstChoice = ''
+    var secondChoice = '';
     
-// }
-
-const addTextToMessage =(title, price) => {
-
    
-    // alert(title+  price)
-
-    // let textarea = document.createElement('textarea');
-    // textarea.setAttribute('name', 'comments')
-    
-    let orders = []
-    let order = {
-        id: Date.now(),
-        title: title,
-        price: price
-    }
-
-for(var i = 0; i < orders.length; i++) {
-    order = order[i]
-    orders.push(order)
-    console.log(orders)
-
-}
-   
-    // orders[orders.length] = title + price
-    // console.log(orders)
-    // for(var i = 0; i < orders.length; i++) {
-      
-        // document.getElementById('orderHere').value=
-        // `<div>${title} ${price}</div>` 
-    // }
-   
-  
-//    orders.push(item)
-
-//    orders.length(price)
-    // textarea.innerText =`${title} `
-    // let orderHere = document.getElementsByClassName('orderHere');
-    // orderHere.append(textarea2804)
-   
-// console.log('item here' + itemHere);
-
-console.log('order' + order.title )
-
-// Store
-localStorage.setItem("title", title);
-localStorage.setItem("price", price);
-
-// Retrieve
-// document.getElementById("orderHere").innerHTML = localStorage.getItem("title" + "price");
-
-    // for(var i = 0; i< order.length; i++) {
-        // console.log(order.length)
-        // alert(order[i])
-        // console.log(order)
-     
-        // let item = document.getElementById('orderHere')[0];
-        // item.value = order[i]
-    }
-    
-    // let orderRow = document.createElement('div')
-    // let order = document.getElementsByClassName('orderHere')[0];
-    // let orderContents = `
-    // <div class="cart-row"><span>${title}</span> ${" "} <span>${price}</span> </div> ` 
-// console.log(orderContents)
-
-// orderRow.innerHTML = orderContents
-
-
-  
-
-//    orderRow.innerHTML = orderContents;
-//    order.append(orderRow)
-    // console.log("fuckin" + item)
-    // order.push(item)
-    // console.log('this is the order' + order)
-    // let i;
-    // for(i = 0; i < order.length; i++) {
-    //     document.getElementById('orderHere').innerText = order[i]
-    // }
-   
-   
-  // alert('done')
-
-// }
-
-// let addItem = document.getElementsByClassName('add-item')
-// addItem.addEventListener('click',function() {
-//     alert('clicked')
-// })
-
-function checkThem() {
-    const checkboxes = document.getElementsByClassName('hide-from-view');
-    // for(var i; i< checkboxes.length; i++) {
-    //     checkboxes = checkboxes[i]
-    // }
-    checkboxes.addEventListener('change',(e)=> {
-        if(e.target.checked){
-            alert('checked')
-        } else {
-            alert('fuck')
-        }
-
+    $('.firstChoice:checked').each(function() {
+        firstChoice+= "First Side: " + $(this).val()
+        console.log(firstChoice)
     })
-}
+    $('.secondChoice:checked').each(function() {
+        secondChoice+= "Second Side: " + $(this).val()
+        console.log(secondChoice)
+    })
+
+})
 
 
-// let order = []
-
-// for(var i; i < checkboxes.length; i++) {
-//     checkboxes[i].addEventListener('click', addToMessage)
-// }
-
-function addToMessage(event) {
-    // alert('checked')
-// let title = event.target.name;
-// console.log(title)
-}
-
-// for(const button of addItem) {
-//     button.addEventListener('click', ()=> {
-//         alert('click')
-//     })
-// }
-
-// addItem.forEach(function(checkbox) {
-//     checkbox.addEventListener('change', function(){
-//         alert('checked')
-//     })
-// })
 
 
 const toTop = document.querySelector('.scroll-top')
